8482eb3c559a7a58b7a84523cbc0fdfc4d1a2f37
AlonPeleg <peleg619@gmail.com>
2019-08-04 18:45:26 +0300

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -33,8 +33,8 @@ const App = () => {
                 <Switch>
                   <PrivateRoute exact path="/" component={Home} />
                   <Route exact path="/about" component={About} />
-                  <Route  path="/register" component={Register} />
-                  <Route  path="/login" component={Login} />
+                  <Route exact path="/register" component={Register} />
+                  <Route exact path="/login" component={Login} />
                 </Switch>
               </div>
             </Fragment>
